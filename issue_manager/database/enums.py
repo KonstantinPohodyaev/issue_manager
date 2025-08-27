89a510f313145d4c13897978e56815544f74cc77
kspohodyaev <konstantinpohodyaev@yandex.ru>
2025-08-26 21:43:03 -0700

    Create Task model

---
+++ issue_manager/database/enums.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class StatusEnum(StrEnum):
+    CREATED: 'created'
+    IN_PROGRESS: 'in_progress'
+    COMPLETED: 'completed'
---
+++ issue_manager/models/task.py
@@ -0,0 +1,21 @@
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from issue_manager.models.base import BaseModel
+from issue_manager.database.enums import StatusEnum
+
+
+class Task(BaseModel):
+    uuid: Mapped[UUID] = mapped_column(
+        primary_key=True, unique=True
+    )
+    title: Mapped[str] = mapped_column(
+        nullable=False
+    )
+    description: Mapped[str] = mapped_column(
+        nullable=True
+    )
+    status: Mapped[StatusEnum] = mapped_column(
+        nullable=False
+    )
